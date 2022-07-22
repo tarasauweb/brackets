66673f936e2164a05f9a2ab9f6b75065b201e29c
Artur Tarasau <tarasov.a.web.@gmail.com>
2022-07-22 09:54:14 +0300

    complite task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = new Map(bracketsConfig)
+    let res = []
+    str = str.split('')
+    for(let key = 0 ; key<str.length;key++){
+        if(str[key]!==brackets.get(res[res.length-1])){
+            res.push(str[key])
+        }
+        else{
+            res.pop()
+        }
+    }
+    if(res.length === 0) {
+        return true
+    }
+    else{
+        return false
+    }
 }
